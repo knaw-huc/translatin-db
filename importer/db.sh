4231df3c5171018e9e8e80c0552c7b2aa8e9c395
HDJ <hayco.de.jong@di.huc.knaw.nl>
2021-10-06 13:25:46 +0200

    improve 'pgserv.conf' generation

--- importer/db.sh
+++ importer/db.sh
@@ -1,9 +1,11 @@
 set -x
 
-grep -C4 db config.ini \
-	| tail -5 \
-	| sed -e 's/: /=/' \
-	      -e 's/database/dbname/' \
-> pgserv.conf
+[ -r pgserv.conf ] || \
+	(grep -C4 db config.ini \
+		| tail -5 \
+		| sed -e 's/: /=/' \
+		      -e 's/database/dbname/' \
+		> pgserv.conf \
+		&& chmod 0400 pgserv.conf)
 
 PGSERVICEFILE=pgserv.conf psql service=db
