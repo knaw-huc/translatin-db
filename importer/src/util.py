977ef989319a67f86650753d81d1af8263d4c5b8
HDJ <hayco@users.noreply.github.com>
2022-05-16 13:03:06 +0200

    Add sha224summer for file contents

--- importer/src/util.py
+++ importer/src/util.py
@@ -1,3 +1,4 @@
+import hashlib
 from icecream import ic
 
 
@@ -12,3 +13,13 @@ def fix_duplicates(origin, original):
     if normalised != original:
         ic('FIXING DUPLICATE: ', origin, original)
     return normalised
+
+
+def sha224sum(filename):
+    h  = hashlib.sha224()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        while n:= f.readinto(mv):
+            h.update(mv[:n])
+    return h.hexdigest()
