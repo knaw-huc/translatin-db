86981f710310d4d6a98d7d7634efcc00bdae4a77
HDJ <hayco@users.noreply.github.com>
2022-06-20 12:53:45 +0200

    Simply query using 'distinct' and fix some bugs

--- importer/src/collate-similar-manifestations.sql
+++ importer/src/collate-similar-manifestations.sql
@@ -2,57 +2,49 @@ SELECT *
 FROM crosstab(
     $$
 with main as (
-SELECT
-    -- stuff related to first candidate
+SELECT distinct
+    -- first manifestation candidate
     M1.ORIGIN ORIGIN1,
-    M1.FORM FORM1,
-    M1.GENRE GENRE1,
-    M1.SUBGENRE SUBGENRE1,
-    M1.EARLIEST EARLIEST1,
-    M1.LATEST LATEST1,
-    MT1.TITLE TITLE1,
-    MT1.LANGUAGE LANG1,
-    MT1.CERTAINTY CERTAIN1,
+    -- order by origin, e.g. (string) "M2008": seen as (int) 2008 => so "M2008", "M700", "M37" is ordered: M37, M700, M2008
+    (SUBSTRING(M1.ORIGIN, 2, LENGTH(M1.ORIGIN)-1))::int AS order1,
 
-    -- stuff related to second candidate
+    -- second manifestation candidate
     M2.ORIGIN ORIGIN2,
-    M2.FORM FORM2,
-    M2.GENRE GENRE2,
-    M2.SUBGENRE SUBGENRE2,
-    M2.EARLIEST EARLIEST2,
-    M2.LATEST LATEST2,
-    MT2.TITLE TITLE2,
-    MT2.LANGUAGE LANG2,
-    MT2.CERTAINTY CERTAIN2
-    
+    (SUBSTRING(M2.ORIGIN, 2, LENGTH(M2.ORIGIN)-1))::int AS order2
+
 FROM
     AUTHORS_MANIFESTATIONS AM1
     JOIN MANIFESTATIONS M1 ON (M1.ID = AM1.MANIFESTATION_ID)
     JOIN MANIFESTATION_TITLES MT1 ON (M1.ID = MT1.MANIFESTATION_ID),
-    
+
     AUTHORS_MANIFESTATIONS AM2
     JOIN MANIFESTATIONS M2 ON (M2.ID = AM2.MANIFESTATION_ID)
     JOIN MANIFESTATION_TITLES MT2 ON (M2.ID = MT2.MANIFESTATION_ID)
-    
+
 WHERE
     -- Find different manifestation candidates
     AM1.MANIFESTATION_ID <> AM2.MANIFESTATION_ID
 
-    -- All candidate1 authors MUST be present as candidate2 authors, however candidate2 MAY have more authors.
+    -- Authors: alle auteurs moeten overeenkomen. Volgorde speelt geen rol.
     -- implementation:
-    --   1) select all candidate1 authors;
-    --   2) remove all candidate2 authors from this set;
-    --   3) answer: candidates are a match if result is empty.
-    AND NOT EXISTS (
-        SELECT AUTHOR_ID
-            FROM AUTHORS_MANIFESTATIONS AM
-            WHERE AM.MANIFESTATION_ID = AM1.MANIFESTATION_ID
-        EXCEPT SELECT AUTHOR_ID
-            FROM AUTHORS_MANIFESTATIONS AM
-            WHERE AM.MANIFESTATION_ID = AM2.MANIFESTATION_ID
+    --   1) candidate1 and candidate2 must have equal number of authors;
+    --   2) select all candidate1 authors;
+    --   3) remove all candidate2 authors from this set;
+    --   4) answer: candidates are a match if result is empty.
+    AND (
+        (SELECT COUNT(AUTHOR_ID) FROM AUTHORS_MANIFESTATIONS WHERE MANIFESTATION_ID = AM1.MANIFESTATION_ID)
+        = (SELECT COUNT(AUTHOR_ID) FROM AUTHORS_MANIFESTATIONS WHERE MANIFESTATION_ID = AM2.MANIFESTATION_ID)
+        AND NOT EXISTS (
+            SELECT AUTHOR_ID
+                FROM AUTHORS_MANIFESTATIONS
+                WHERE MANIFESTATION_ID = AM1.MANIFESTATION_ID
+            EXCEPT SELECT AUTHOR_ID
+                FROM AUTHORS_MANIFESTATIONS
+                WHERE MANIFESTATION_ID = AM2.MANIFESTATION_ID
+        )
     )
-                
-    -- Titles: Alle talen van de Manifestation (ongeacht de zekerheid waarmee deze zijn vastgesteld) moeten overeenkomen. 
+
+    -- Titles: Alle talen van de Manifestation (ongeacht de zekerheid waarmee deze zijn vastgesteld) moeten overeenkomen.
     -- De volgorde speelt hierbij geen rol geen rol.
     -- implementation:
     --   1) candidate1 and candidate2 must have equal number of titles;
@@ -60,15 +52,15 @@ WHERE
     --   3) remove all candidate2 titles;
     --   4) answer: candidates match if result is empty
     AND (
-        (select count(language) from manifestation_titles mt where mt.manifestation_id = am1.manifestation_id)
-         = (select count(language) from manifestation_titles mt where mt.manifestation_id = am2.manifestation_id)
+        (SELECT COUNT(LANGUAGE) FROM MANIFESTATION_TITLES WHERE MANIFESTATION_ID = MT1.MANIFESTATION_ID)
+         = (SELECT COUNT(LANGUAGE) FROM MANIFESTATION_TITLES WHERE MANIFESTATION_ID = MT2.MANIFESTATION_ID)
         AND NOT EXISTS (
             SELECT LANGUAGE
-                FROM MANIFESTATION_TITLES MT
-                WHERE MT.MANIFESTATION_ID = AM1.MANIFESTATION_ID
+                FROM MANIFESTATION_TITLES
+                WHERE MANIFESTATION_ID = MT1.MANIFESTATION_ID
             EXCEPT SELECT LANGUAGE
-                FROM MANIFESTATION_TITLES MT
-                WHERE MT.MANIFESTATION_ID = AM2.MANIFESTATION_ID
+                FROM MANIFESTATION_TITLES
+                WHERE MANIFESTATION_ID = MT2.MANIFESTATION_ID
         )
     )
 
@@ -82,29 +74,27 @@ WHERE
 
     -- Form_type must match
     AND M1.FORM_TYPE = M2.FORM_TYPE
-    
+
     -- Genre must match, if it exists
     AND (
         M1.GENRE = M2.GENRE
         OR M1.GENRE IS NULL
         OR M2.GENRE IS NULL
     )
-    
+
     -- Subgenre must match, if it exists
     AND (
         M1.SUBGENRE = M2.SUBGENRE
         OR M1.SUBGENRE IS NULL
         OR M2.SUBGENRE IS NULL
     )
-        
+
     -- No need to see each symmetrical pair (origin1,origin2) again as (origin2,origin1)
     -- e.g. from (M24,M31) and (M31,M24) only keep (M24,M31) based on 24 < 31
     AND (SUBSTRING(M1.ORIGIN, 2, LENGTH(M1.ORIGIN)-1))::int < (SUBSTRING(M2.ORIGIN, 2, LENGTH(M2.ORIGIN)-1))::int
 
 ORDER BY
-    -- order by origin, e.g. (string) "M2008": seen as (int) 2008 => so "M2008", "M700", "M37" is ordered: M37, M700, M2008
-    (SUBSTRING(M1.ORIGIN, 2, LENGTH(M1.ORIGIN)-1))::int,  -- first origin1
-    (SUBSTRING(M2.ORIGIN, 2, LENGTH(M2.ORIGIN)-1))::int   -- then origin2
+    order1, order2
 )
 select origin1, 'category_not_used', origin2
 from main m1
